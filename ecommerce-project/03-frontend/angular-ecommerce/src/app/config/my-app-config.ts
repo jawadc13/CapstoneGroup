b9496e87825361e49ba80c35e93e05dbf91b96b0
Jawad Chowdhury <jawadc13@gmail.com>
2022-03-16 05:40:20 -0500

    finally adding frontend

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa46dqa7ktd5NYlU5d7',
+        issuer: 'https://dev-86130784.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
