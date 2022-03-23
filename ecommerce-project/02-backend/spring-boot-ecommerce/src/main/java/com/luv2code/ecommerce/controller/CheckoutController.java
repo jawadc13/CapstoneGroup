3f38061b6b9857f81e06ac08812b2df539274459
shanejr7 <shane.jr7@icloud.com>
2022-03-23 15:50:06 -0500

    backend update

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/controller/CheckoutController.java
@@ -1,18 +1,28 @@
 package com.luv2code.ecommerce.controller;
 
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.service.CheckoutService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 @RestController
-@RequestMapping("/checkout")
+@RequestMapping("api/checkout")
 public class CheckoutController {
 	
+	private Logger logger = Logger.getLogger(getClass().getName());
+
 	private CheckoutService checkoutService;
 	
 	public CheckoutController(CheckoutService checkoutService) {
@@ -25,5 +35,17 @@ public class CheckoutController {
 		
 		return purchaseResponse;
 	}
+	
+	@PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfo paymentInfo) throws StripeException {
+
+	
+		logger.info("paymentInfo.amount: "+ paymentInfo.getAmount());
+        PaymentIntent paymentIntent = checkoutService.createPaymentIntent(paymentInfo);
+
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
 
 }
--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,17 +1,27 @@
 package com.luv2code.ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+
 import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 
 import jakarta.transaction.Transactional;
 @Service
@@ -21,8 +31,11 @@ public class CheckoutServiceImpl implements CheckoutService {
 	private CustomerRepository customerRepository;
 	
 	@Autowired
-	public CheckoutServiceImpl(CustomerRepository customerRepository) {
+	public CheckoutServiceImpl(CustomerRepository customerRepository, @Value("${stripe.key.secret}") String secretKey) {
 		this.customerRepository = customerRepository;
+
+		// initialize Stripe API with secret key
+		Stripe.apiKey = secretKey;
 	}
 	
 	@Override
@@ -73,5 +86,22 @@ public class CheckoutServiceImpl implements CheckoutService {
 		
 		return UUID.randomUUID().toString(); 
 	}
+	
+	@Override
+	public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+
+		List<String> paymentMethodTypes = new ArrayList<>();
+		paymentMethodTypes.add("card");
+
+		Map<String, Object> params = new HashMap<>();
+		params.put("amount", paymentInfo.getAmount());
+		params.put("currency", paymentInfo.getCurrency());
+		params.put("payment_method_types", paymentMethodTypes);
+		params.put("description", "purchase");
+		params.put("receipt_email", paymentInfo.getReceiptEmail());
+
+		return PaymentIntent.create(params);
+	}
+
 
 }
