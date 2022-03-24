a0a9c63ad3d72f2b296751c8d62306e75590e543
shanejr7 <shane.jr7@icloud.com>
2022-03-24 11:27:32 -0500

    backend okta security

--- ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
+++ ecommerce-project/02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/MyDataRestConfig.java
@@ -13,6 +13,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 import com.luv2code.ecommerce.entity.Country;
+import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.Product;
 import com.luv2code.ecommerce.entity.ProductCategory;
 import com.luv2code.ecommerce.entity.State;
@@ -22,75 +23,62 @@ import jakarta.persistence.metamodel.EntityType;
 
 @Configuration
 public class MyDataRestConfig implements RepositoryRestConfigurer {
-	
+
 	@Value("${allowed.origins}")
 	private String[] theAllowedOrigins;
-	
+
 	private EntityManager entityManager;
-	
+
 	@Autowired
 	public MyDataRestConfig(EntityManager theEntityManager) {
 		entityManager = theEntityManager;
 	}
-	
-	
-	
-	
-	
-	
+
 	@Override
 	public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
-		
-		HttpMethod[] theUnsupportedActions = {HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH};
-		
-		//disable http methods for Product: PUT, POST and DELETE
-		disableHttpMethods(Product.class, config, theUnsupportedActions);
-		
-		//disable http methods for ProductCategory: PUT, POST and DELETE
-		disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
-		
 
-				disableHttpMethods(Country.class, config, theUnsupportedActions);
-				disableHttpMethods(State.class, config, theUnsupportedActions);
-		
-		
+		HttpMethod[] theUnsupportedActions = { HttpMethod.PUT, HttpMethod.POST, HttpMethod.DELETE, HttpMethod.PATCH };
+
+		// disable http methods for Product: PUT, POST and DELETE
+		disableHttpMethods(Product.class, config, theUnsupportedActions);
+
+		// disable http methods for ProductCategory: PUT, POST and DELETE
+		disableHttpMethods(ProductCategory.class, config, theUnsupportedActions);
+
+		disableHttpMethods(Country.class, config, theUnsupportedActions);
+		disableHttpMethods(State.class, config, theUnsupportedActions);
+		disableHttpMethods(Order.class, config, theUnsupportedActions);
+
 		// call an internal helper method
 		exposeIds(config);
-		
-		//configure cors mapping
+
+		// configure cors mapping
 		cors.addMapping("/**").allowedOrigins(theAllowedOrigins);
 	}
 
-
-
-
-
-
-	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
-		config.getExposureConfiguration()
-		.forDomainType(theClass)
-		.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
-		.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
+	private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config,
+			HttpMethod[] theUnsupportedActions) {
+		config.getExposureConfiguration().forDomainType(theClass)
+				.withItemExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions))
+				.withCollectionExposure((metdata, httpMethods) -> httpMethods.disable(theUnsupportedActions));
 	}
-
 
 	private void exposeIds(RepositoryRestConfiguration config) {
 		// TODO Auto-generated method stub
-		
-		
-		//expose entity ids
-		
+
+		// expose entity ids
+
 		// - get a list of all entity classes from the entity manager
 		Set<EntityType<?>> entities = entityManager.getMetamodel().getEntities();
-		
+
 		// - create an array of the entity types
 		List<Class> entityClasses = new ArrayList<>();
-		
+
 		// - get the entity types for the entities
 		for (EntityType tempEntityType : entities) {
 			entityClasses.add(tempEntityType.getJavaType());
 		}
-		
+
 		// - expose the entity ids for the array of entity/domain types
 		Class[] domainTypes = entityClasses.toArray(new Class[0]);
 		config.exposeIdsFor(domainTypes);
