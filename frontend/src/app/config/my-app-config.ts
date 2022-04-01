a71d01affd8fb6f6d6a42f4fa039c11124c6a15e
Jawad Chowdhury <jawadc13@gmail.com>
2022-04-01 15:03:31 -0500

    b

--- frontend/src/app/config/my-app-config.ts
+++ frontend/src/app/config/my-app-config.ts
@@ -1,8 +1,8 @@
 export default {
 
     oidc: {
-        clientId: '0oa46dqa7ktd5NYlU5d7',
-        issuer: 'https://dev-86130784.okta.com/oauth2/default',
+        clientId: '0oa44pmi93YF2AgDv5d7',
+        issuer: 'https://dev-8620863.okta.com/oauth2/default',
         redirectUri: 'https://ecommercefront.azurewebsites.net/login/callback',
         scopes: ['openid', 'profile', 'email']
     }
@@ -17,4 +17,5 @@ export default {
     scopes: ['openid', 'profile', 'email']
     }
     }
-    */
+    
+*/
