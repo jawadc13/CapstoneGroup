0ab3888053deb15c0b050f3c4b4efad3910720a1
Jawad Chowdhury <jawadc13@gmail.com>
2022-04-12 15:53:12 -0500

    changes to configuration

---
+++ backend/src/main/java/com/luv2code/ecommerce/config/MultiSecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.okta.spring.boot.oauth.Okta;
+
+public class MultiSecurityConfiguration {
+
+    @Configuration
+    public static class mainConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+
+            // protect endpoint /api/orders
+            http.authorizeRequests()
+                    .antMatchers("/api/orders/**")
+                    .authenticated()
+                    .and()
+                    .oauth2ResourceServer()
+                    .jwt();
+
+            // add CORS filters
+            http.cors();
+
+            // force a non-empty response body for 401's to make the response more friendly
+            Okta.configureResourceServer401ResponseBody(http);
+
+            // disable CSRF since we are not using Cookies for session tracking
+            http.csrf().disable();
+        }
+    }
+
+    public static class swaggerConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/swagger-ui.html", "/swagger-ui/index.html");
+        }
+    }
+
+}
